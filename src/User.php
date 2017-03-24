4219cbf69f4aa82775d7099846a895f707bda55a
Jakub <jakub@student.edu>
2017-03-24 16:58:58 +0100

    Creating User class

--- src/User.php
+++ src/User.php
@@ -1,1 +1,22 @@
+<?php
 
+class User extends DbModel 
+{
+    private $id;
+    private $email;
+    private $name;
+    private $surname;
+    private $pass;
+    private $addres;
+    
+    private function __construct($newId, $newName, $newSurname, $newEmail, $newAddress, $newPassword){
+        $this->id = $newId;
+        $this->name = $newName;
+        $this->surname = $newSurname;
+        $this->email = $newEmail;
+        $this->addres= $newAddress;
+        $this->password = $newPassword;
+    }
+    
+    
+}
